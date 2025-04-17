9b9c31fd30ce171cd63ab68d75c2e664c6021328
abhishek1501kholiya <abhishekkholiya15012007@gmail.com>
2025-04-17 17:09:05 +0530

    fixed research page and api calls

--- README.md
+++ README.md
@@ -1,40 +1,20 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/pages/api-reference/create-next-app).
+#Vakeel AI  
+The legal assistant you never knew but always wanted  
 
-## Getting Started
+##Key Features  
+- Deep Legal Research (Search through lakhs of real court judgments across the Supreme Court, High Courts, and District Courts.)  
+- Ai Case Summaries (No more legal jargon. Easy to understand summaries tailored for lawyers & common man.)  
+- Ask questions in simple language (“find me a fake domestic violence case”)  
+- Auto Generated Case Notes ("Converts your research into citation ready notes. Saving hours of prep for lawyers.")  
 
-First, run the development server:
+##Tech Stack  
+- Frontend (Next.js & CSS)  
+- Authentication & Storage (Firebase & MongoDB)  
+- Ai & Apis (Gemini & Indian Kanoon)  
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+##Subscription Plans -   
+- Common Man (0 rupees per month)  
+- Attorney's plan (499 per month)  
+- Enterprise's plan (399 per member)  
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
-
-[API routes](https://nextjs.org/docs/pages/building-your-application/routing/api-routes) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.js`.
-
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/pages/building-your-application/routing/api-routes) instead of React pages.
-
-This project uses [`next/font`](https://nextjs.org/docs/pages/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn-pages-router) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/pages/building-your-application/deploying) for more details.
+Made with love by ashish nagar and abhishek kholiya
---
+++ pages/research/[chat].js
@@ -0,0 +1,208 @@
+import styles from '@/styles/ChatRoom.module.css';
+import { useState } from 'react';
+import Head from 'next/head';
+import { GoogleGenAI, GoogleGenerativeAI } from "@google/genai";
+const genAI = new GoogleGenAI({ apiKey: process.env.NEXT_PUBLIC_GEMINI_API });
+import Markdown from 'react-markdown';
+export default function chat(){
+
+    const [query,setQuery] = useState('');
+    const [messages,setMessages] = useState(null);
+    const [cases,setCases] = useState(null);
+    const [popUp,setPopUp] = useState(false);
+
+
+    const queryAI = async (prompt)=> {
+        
+        try{
+                    //this code is responsible
+                            const response2 = await fetch(`/api/fetchCases?query=${prompt}`);
+                            const data = await response2.json();
+                            // console.log(data);
+
+                        
+
+
+                            // console.log("trying to run the model");
+                            const response = await genAI.models.generateContent({
+                                model: "gemini-2.0-flash",
+                                contents: `summarise this in very short paragraphs (no markdown & no bold text) - ${data.doc}`,
+                            });
+                            
+                        
+                            const result = await response.text;
+
+                            let newResult = 
+                            `
+                                From: <Strong>${data.docsource}</Strong> <br/>
+                                Title: <Strong>${data.title}</Strong> <br/>
+                                Summary: <br/>
+                                ${result}
+                            `;
+
+                            let newCases = cases;
+                            if(cases !== null&&Array.isArray(cases)){
+                                // console.log('happened')
+                                console.log(data);
+                                newCases = [...cases,data];
+                            }else{
+                                // console.log('happened 2')
+                                console.log(data);
+                                newCases = [data];
+                            }
+                            setCases(newCases);
+                            
+                    //
+
+            // let newResult = 
+            // `
+            //     From: <Strong>Supreme Court Of India</Strong> <br/>
+            //     Title: <Strong>Sri Jayendra Saraswathy ... vs State Of Tamil Nadu And Others on 26 October, 2005</Strong> <br/>
+            //     Summary: <br/>
+            //     The Supreme Court heard a petition from Sri Jayendra Saraswathy Swamigal seeking transfer of his criminal case from Tamil Nadu, citing concerns of unfair trial due to state machinery bias. The petitioner alleged state actions like statements by the Chief Minister, solatium payment to the victim's widow, false cases against co-accused and lawyers, and freezing of Mutt accounts created an atmosphere of bias. The Court found merit in the petitioner's concerns, highlighting the state's actions against lawyers, freezing of Mutt accounts and detention of co-accused. The Court acknowledged a reasonable apprehension of unfair trial in Tamil Nadu. The Court allowed the transfer petition, moving the case to the Principal District and Sessions Judge, Pondicherry, citing language familiarity and proximity to the original location as factors.
+            // `
+
+            let newMessages;
+            if(messages !== null){
+                // console.log('not null 2');
+                newMessages  = [...messages,{content:newResult,sender:'ai'}];
+            }else{
+                // console.log('null 2');
+                newMessages = [{content:prompt,sender:'human'},{content:newResult,sender:'ai'}];
+            }
+            // console.log("newCases that was set:", newCases); 
+            return setMessages(newMessages);
+
+
+
+            
+
+
+        }catch(err){
+            console.log(err);
+            return "error either your prompt is against the guidelines or something went wrong";
+        }
+    }
+
+    const handleKeyPress = async (e)=>{
+        if(e.key === 'Enter'){
+            let newQuery = query;
+            setQuery('');
+            let currentMessages = messages;
+            if(currentMessages !== null){
+                // console.log("not null")
+                currentMessages.push({content:newQuery,sender:'human'});
+                setMessages(currentMessages);
+            }else{
+                // console.log("null")
+                setMessages([{content:newQuery,sender:'human'}]);
+            }
+            await queryAI(newQuery);
+           
+        }
+    }
+
+    const handleInputChange = (e)=>{
+       
+            let queryValue = String(e.target.value);
+            setQuery(queryValue);
+        
+    }
+    return(
+        <>
+            <Head>
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+            <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Istok+Web:ital,wght@0,400;0,700;1,400;1,700&family=Joan&display=swap" rel="stylesheet"></link>
+            </Head>
+            <div className={styles.container}>
+                <div className={styles.leftSection}>
+
+                </div>
+                <div className={styles.rightSection}>
+
+                        <div className={styles.rightSection_top}>
+                                <h2 className={styles.rightSection_header}>Vakeel Ai</h2>
+                                <img className={styles.rightSection_profilePicture} src='/profilepicture.png' width={40} height={40}/>
+                        </div>
+
+                        <div className={styles.rightSection_chat}>
+                            <div className={styles.rightSection_chat_content}>
+                                    {messages && messages.length >= 1 &&  messages.map((i,index)=>{
+                                        return(
+                                            <>
+                                                <div className={styles.message_div}>
+                                                
+                                                    {messages[index].sender === 'human' ?
+                                                        <div className={styles.message_content}>
+                                                             <p>{messages[index].content}</p>
+                                                        </div>
+                                                        :
+                                                        <div className={styles.ai_message_div}>
+                                                          
+                                                            <div className={styles.message_content_ai_left}>
+                                                                   <img src='/shining.png' width={20} height={20}/>
+                                                                   <p className={styles.message_content_ai_title}>Generated</p>
+                                                           </div>
+                                                            <p className={styles.ai_message} dangerouslySetInnerHTML={{ __html: messages[index].content }} />
+                                                            <button onClick={()=>setPopUp(true)} className={styles.ai_message_more_link}>View More</button>
+
+                                                            {popUp ?
+                                                                    <>
+                                                                        <div className={styles.popUp_container}>
+                                                                                <div className={styles.popUp_div}>
+                                                                                    <h2 className={styles.popUp_div_title}>{cases[cases.length - 1].title}</h2>
+                                                                                    <p className={styles.popUp_div_paragraph}>Source: {cases[cases.length -1].docsource}</p>
+                                                                                    <p className={styles.popUp_div_paragraph}>Dated: {cases[cases.length - 1].publishdate}</p>
+
+                                                                                    {/* <div className={styles.popUpdiv_bottom}> */}
+                                                                                        <div className={styles.popUp_div_document}>
+                                                                                            <p dangerouslySetInnerHTML={{ __html: cases[cases.length -1].doc }}/>
+                                                                                        </div>
+                                                                                        <button className={styles.popUp_div_button} onClick={()=>setPopUp(false)}>
+                                                                                            Cancel
+                                                                                        </button>
+                                                                                    {/* </div> */}
+                                                                                  
+
+
+                                                                                </div>
+
+                                                                        </div>
+                                                                    </>
+                                                                    :
+                                                                    <>
+
+
+                                                                    </>
+
+                                                            }
+                                            
+                                                        </div>
+                                                     
+                                                    }
+
+
+                                                </div>
+
+                                               
+                                            
+                                            </>
+                                        )
+                                    })}
+                            </div>
+
+                           
+                        </div>
+
+                        <div className={styles.rightSection_bottom}>
+                                <div className={styles.rightSection_inputDiv} >
+
+                                    <textarea className={styles.rightSection_input} onChange={handleInputChange} placeholder='Research' onKeyPress={handleKeyPress} value={query}/>
+                                </div>
+                        </div>
+                </div>
+            </div>
+        </>
+    )
+}
